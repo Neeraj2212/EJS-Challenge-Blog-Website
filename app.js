3c3ec3408fd5c72a93d97273ea8949004ffa9405
Neeraj Patil <patilneeraj2212@gmail.com>
2020-10-16 22:05:58 +0530

    Blog Website using EJS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const _ = require('lodash');
 
 const homeStartingContent =
 	'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -48,6 +49,16 @@ app.post('/compose', function (req, res) {
 	posts.push(temp);
 
 	res.redirect('/');
+});
+
+app.get('/posts/:postTitle', function (req, res) {
+	reqPost = _.camelCase(req.params.postTitle);
+
+	posts.forEach((element) => {
+		if (_.camelCase(element.title) === reqPost) {
+			res.render('post', { dynamicPost: element });
+		}
+	});
 });
 
 app.listen(3000, function () {
